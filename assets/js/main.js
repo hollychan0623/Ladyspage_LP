c31c881b7158e7913e7b1a2b8752dbedfded8f9c
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-08-08 17:24:32 +0900

    グッチ加工画像、品名挿入

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,28 @@ $(function(){
     });
   });
 
- $(document). ready(function(){
-   $('.items').fadeIn(1500);
- })
+  $(function(){
+    function animation(){
+      $('.items').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
+//  $(document). ready(function(){
+//    $('.items').fadeIn(1500);
+//  })
