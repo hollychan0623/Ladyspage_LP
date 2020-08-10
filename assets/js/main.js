426f3e33df9856619be23e79933818ff2d2623fd
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-08-10 17:27:49 +0900

    LV,シャネル画像作成
    フレーズのアニメーションスクロールに付随するように変更すること

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,15 @@ $(function(){
 //  $(document). ready(function(){
 //    $('.items').fadeIn(1500);
 //  })
+
+$(function() {
+  var $allMsg = $('#title');
+  var $wordList = $('#title').html().split("");
+  $('#title').html("");
+  $.each($wordList, function(idx, elem) {
+      var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+      newEL.appendTo($allMsg);
+      newEL.delay(idx * 100);
+      newEL.animate({ opacity: 1 }, 2000);
+  });
+});
