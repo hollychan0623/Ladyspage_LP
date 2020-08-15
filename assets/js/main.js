6b6eccc8b90135558d95e1bef389444efed0d5c6
hollychan0623 <56242205+hollychan0623@users.noreply.github.com>
2020-08-15 12:35:04 +0900

    財布コーディング・画像設置
    PC版の画像とコーディング財布までは終了

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,28 @@ $(function(){
     });
   });
 
+  $(function(){
+    function animation(){
+      $('.wallet_items').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
+  });
+
 //  $(document). ready(function(){
 //    $('.items').fadeIn(1500);
 //  })
